d068d62bc3fdb9ccd469d6dbc4769b1238d0d7bb
Jodi Kurniawan <jodikr8s@gmail.com>
2023-09-18 23:28:19 +0700

    Add testing use case for check data get in usecase from repository or not.

--- app/src/main/java/com/example/cleanarchitecture/domain/MessageUseCase.kt
+++ app/src/main/java/com/example/cleanarchitecture/domain/MessageUseCase.kt
@@ -1,5 +1,10 @@
 package com.example.cleanarchitecture.domain
 
+
+//This class is used as the parent code for the Interactor to be defined.
+//Why do you need to create this interface?
+//This is because the relationship between ViewModel/Presenter and UseCase should
+//not be from concrete class to concrete class, but from concrete class to abstract class.
 interface MessageUseCase {
     fun getMessage(name: String): MessageEntity
 }
